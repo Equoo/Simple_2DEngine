b972c4bec2a183bd0dca1ae3bea7de771d371dbc
Equo <biobraix@outlook.fr>
2022-07-17 04:14:01 +0200

    Starting

---
+++ Engine/Simple_2DEngine.cpp
@@ -0,0 +1,12 @@
+﻿// Simple_2DEngine.cpp : définit le point d'entrée de l'application.
+//
+
+#include "Simple_2DEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
